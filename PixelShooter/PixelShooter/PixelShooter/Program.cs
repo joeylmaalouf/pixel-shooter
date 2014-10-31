682e1d4c842072b3fc72ce94322a121297e4b729
Joey L. Maalouf <joeylmaalouf@gmail.com>
2014-10-31 15:12:23 -0400

    added farseer physics engine

--- PixelShooter/PixelShooter/PixelShooter/Program.cs
+++ PixelShooter/PixelShooter/PixelShooter/Program.cs
@@ -5,12 +5,9 @@ namespace PixelShooter
 #if WINDOWS || XBOX
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         static void Main(string[] args)
         {
-            using (Game1 game = new Game1())
+            using (GamePixelShooter game = new GamePixelShooter())
             {
                 game.Run();
             }
@@ -18,4 +15,3 @@ namespace PixelShooter
     }
 #endif
 }
-
